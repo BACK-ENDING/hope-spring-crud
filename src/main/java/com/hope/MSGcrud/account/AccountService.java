da4a961cdbd450a2c5d439764a1e6ca218e5f492
KIMHUEMANG <piyrw9754@gmail.com>
2022-09-15 08:03:56 +0900

    repo에 의존하고있던 controller Service를 통해 분리

--- src/main/java/com/hope/MSGcrud/account/AccountService.java
+++ src/main/java/com/hope/MSGcrud/account/AccountService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AcountService {
+public class AccountService {
 
     @Autowired
     private JdbcAccountRepository jdbcAccountRepository;
--- src/main/java/com/hope/MSGcrud/controller/AccountController.java
+++ src/main/java/com/hope/MSGcrud/controller/AccountController.java
@@ -1,9 +1,8 @@
 package com.hope.MSGcrud.controller;
 
 import com.hope.MSGcrud.account.Account;
-import com.hope.MSGcrud.account.JdbcAccountRepository;
+import com.hope.MSGcrud.account.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AccountController {
 
     @Autowired
-    JdbcAccountRepository jdbcAccountRepository;
+    AccountService accountService;
 
     @GetMapping("/list") // 게시글 목록
     public String list(Model model){
 
-        model.addAttribute("accounts", jdbcAccountRepository.findAll());
-        model.addAttribute("count" , jdbcAccountRepository.count());
+        model.addAttribute("accounts", accountService.findAll());
+        model.addAttribute("count" , accountService.count());
 
         return "account/list";
     }
@@ -34,14 +33,14 @@ public class AccountController {
 
     @PostMapping("/add") //추가 처리
     public String addProcess(String email){
-        jdbcAccountRepository.save(new Account(email));
+        accountService.save(new Account(email));
 
         return "redirect:/account/list";
     }
 
     @GetMapping("/{id}") //상세보기
     public String view(@PathVariable Long id, Model model) {
-        model.addAttribute("account", jdbcAccountRepository.findById(id));
+        model.addAttribute("account", accountService.findById(id));
 
         return "account/view";
     }
@@ -49,23 +48,23 @@ public class AccountController {
     @GetMapping("/{id}/modify") //수정 Form
     public String modify(@PathVariable Long id, Model model) {
 
-        model.addAttribute("account", jdbcAccountRepository.findById(id));
+        model.addAttribute("account", accountService.findById(id));
 
         return "account/modify";
     }
 
     @PostMapping("/{id}/modify") //수정 처리
     public String modifyProcess(@PathVariable Long id, String email) {
-        Account account = jdbcAccountRepository.findById(id);
+        Account account = accountService.findById(id);
         account.setEmail(email);
-        jdbcAccountRepository.update(account);
+        accountService.update(account);
 
         return String.format("redirect:/account/%d", id) ;
     }
 
     @PostMapping("/{id}/delete") //삭제 처리
     public String deleteProcess(@PathVariable Long id) {
-        jdbcAccountRepository.deleteById(id);
+        accountService.deleteById(id);
 
         return "redirect:/account/list";
     }
